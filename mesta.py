027c3c5e89dd704c40619b8d98fa59e76a8b3864
Tatiana Bartelová <113470730+bartelka@users.noreply.github.com>
2024-01-29 14:05:47 +0100

    Update mesta.py

--- mesta.py
+++ mesta.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 fr_1 = open("vrcholy.txt", "r", encoding="utf-8")
 vrcholy = [i.strip().split(";") for i in fr_1]
@@ -7,9 +8,10 @@ fr_2 = open("hrany.txt", "r", encoding="utf-8")
 hrany = [i.strip().split(";") for i in fr_2]
 
 def kresli_mesto(vrcholy):
-    global mestecka
+    global mestecka, mesta_bodky
     for i in vrcholy:
-        canvas.create_oval(int(i[1]) - 2, int(i[2]) - 2, int(i[1]) + 2, int(i[2]) + 2, fill="hotpink")
+        bodka = canvas.create_oval(int(i[1]) - 4, int(i[2]) - 4, int(i[1]) + 4, int(i[2]) + 4, fill="hotpink")
+        mesta_bodky[i[0]] = mesta_bodky.get(i[0], bodka)
         canvas.create_text(int(i[1]), int(i[2]) + 8, fill="black", text=i[0])
         mestecka[i[0]] = mestecka.get(i[0], (int(i[1]), int(i[2])))
 
@@ -38,6 +40,30 @@ def priradenie_miest(hrany):
             mesta_hodnoty[m_2] = mesta_hodnoty.get(m_2, {})
         mesta_hodnoty[m_2][m_1] = vzdialenost
 
+def into_the_deep(mesto):
+    global navstivene_1
+    navstivene_1.append(mesto)
+    canvas.itemconfig(mesta_bodky[mesto], fill="blue")
+    canvas.update()
+    time.sleep(0.1)
+    for mestecko, vzdialenost in mesta_hodnoty[mesto].items():
+        if mestecko not in navstivene_1:
+            into_the_deep(mestecko)
+
+def into_the_width(mesto):
+    global navstivene
+    zoznamik = []
+    navstivene_2.append(mesto)
+    zoznamik.append(mesto)
+    while zoznamik != []:
+        print(zoznamik)
+        for mesto, hod in mesta_hodnoty[zoznamik[0]].items():
+            if mesto not in navstivene_2:
+                zoznamik.append(mesto)
+                navstivene_2.append(mesto)
+        if len(zoznamik) != 0:
+            zoznamik.remove(zoznamik[0])
+
 
 import tkinter as tk
 
@@ -46,12 +72,18 @@ win = tk.Tk()
 w = 1000
 h = 900
 
+mesta_bodky = {}
 #mestecka = mesta so suradnicami
 mestecka = {}
 #mesta = k mestam su priradene susedne mesta
 mesta = {}
 #mesta so vzdialenostami medzi sebou
 mesta_hodnoty = {}
+#navstivene mesta
+navstivene_1 = []
+navstivene_2 = []
+
+ciary = []
 
 canvas = tk.Canvas(width=w, height=h, bg="white")
 canvas.pack()
@@ -60,7 +92,10 @@ kresli_mesto(vrcholy)
 priradenie_miest(hrany)
 kresli_hrany()
 
-print(mesta_hodnoty)
+into_the_deep("Martin")
+into_the_width("Bratislava")
 
+print(mesta_hodnoty)
+print(mesta_bodky)
 win.mainloop()
 #dudo.gvpt.sk zadanie -> grafy(hrany.txt, vrcholy.txt)
